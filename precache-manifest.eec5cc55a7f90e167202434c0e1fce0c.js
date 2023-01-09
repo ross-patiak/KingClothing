e64315af532fd396f291ca247ba8fa232dbc4dc7
ross-patiak <ross.patiak@gmail.com>
2023-01-08 23:49:48 -0500

    Updates

---
+++ precache-manifest.eec5cc55a7f90e167202434c0e1fce0c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "889ac9fa0b089b76429cc2b8fcba1d64",
+    "url": "/KingClothing/index.html"
+  },
+  {
+    "revision": "bb5839b9ff663e82580a",
+    "url": "/KingClothing/static/css/main.b94863d8.chunk.css"
+  },
+  {
+    "revision": "0227ffca0488d5928348",
+    "url": "/KingClothing/static/js/2.b7e87fde.chunk.js"
+  },
+  {
+    "revision": "dc765324f640aa505a4111deda918327",
+    "url": "/KingClothing/static/js/2.b7e87fde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb5839b9ff663e82580a",
+    "url": "/KingClothing/static/js/main.e699f484.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/KingClothing/static/js/main.e699f484.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31f5ea72ffd09e00938c",
+    "url": "/KingClothing/static/js/runtime-main.a689d686.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/KingClothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/KingClothing/static/media/shopping-bag.09e6f5e7.svg"
+  }
+]);
