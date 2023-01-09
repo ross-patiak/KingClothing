e0a837cd0fc3750e5dd2ca0e47eade979ff52e69
ross-patiak <ross.patiak@gmail.com>
2023-01-09 00:04:57 -0500

    more routing fixes

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,38 +1,38 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-import { auth } from '../../firebase/firebase.utils';
-import CartIcon from '../cart-icon/cart-icon.component';
-import CartDropdown from '../cart-dropdown/cart-dropdown.component';
-import { selectCartHidden } from '../../redux/cart/cart.selectors';
-import { selectCurrentUser } from '../../redux/user/user.selectors';
+import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+import { selectCartHidden } from "../../redux/cart/cart.selectors";
+import { selectCurrentUser } from "../../redux/user/user.selectors";
 
-import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { ReactComponent as Logo } from "../../assets/crown.svg";
 
-import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink, OptionDiv } from './header.styles';
+import {
+  HeaderContainer,
+  LogoContainer,
+  OptionsContainer,
+  OptionLink,
+  OptionDiv,
+} from "./header.styles";
 
 const Header = ({ currentUser, hidden }) => (
   <HeaderContainer>
-    <LogoContainer to='/'>
-      <Logo className='logo' />
+    <LogoContainer to="/KingClothing">
+      <Logo className="logo" />
     </LogoContainer>
-    
+
     <OptionsContainer>
-      <OptionLink to='/shop'>
-        SHOP
-      </OptionLink>
+      <OptionLink to="/KingClothing/shop">SHOP</OptionLink>
       {/* <OptionLink to='/shop'>
         CONTACT
       </OptionLink> */}
       {currentUser ? (
-        <OptionDiv onClick={() => auth.signOut()}>
-          SIGN OUT
-        </OptionDiv>
+        <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
       ) : (
-        <OptionLink to='/signin'>
-          SIGN IN
-        </OptionLink>
+        <OptionLink to="/KingClothing/signin">SIGN IN</OptionLink>
       )}
       <CartIcon />
     </OptionsContainer>
@@ -42,7 +42,7 @@ const Header = ({ currentUser, hidden }) => (
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
-  hidden: selectCartHidden
+  hidden: selectCartHidden,
 });
 
 export default connect(mapStateToProps)(Header);
